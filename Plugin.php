a6cf74bf504bd618caa3d5256b820f24bb557b08
iSuperman <isignacc@outlook.com>
2022-04-24 16:55:06 +0800

    update for typecho1.2 and php8+

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Bark推送评论通知
+ * <strong style="color:#28B7FF;font-family: 楷体;">Bark推送评论通知</strong>
  * 
  * @package Comment2Bark
- * @author Mr.Cola
- * @version 1.0
- * @link https://blog.colaink.com
+ * @author <strong style="color:#28B7FF;font-family: 楷体;">isSuperman</strong>
+ * @version 1.1
+ * @link https://github.com/isSuperman/Typecho-Plugin-Comment2Bark
  */
-class Comment2Bark_Plugin implements Typecho_Plugin_Interface
-{
+class Comment2Bark_Plugin implements Typecho_Plugin_Interface {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
      * 
@@ -16,14 +15,12 @@ class Comment2Bark_Plugin implements Typecho_Plugin_Interface
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function activate()
-    {
-    
-        Typecho_Plugin::factory('Widget_Feedback')->comment = array('Comment2Bark_Plugin', 'sc_send');
-        Typecho_Plugin::factory('Widget_Feedback')->trackback = array('Comment2Bark_Plugin', 'sc_send');
-        Typecho_Plugin::factory('Widget_XmlRpc')->pingback = array('Comment2Bark_Plugin', 'sc_send');
+    public static function activate() {
+        Typecho_Plugin::factory('Widget_Feedback')->comment = array('Comment2Bark_Plugin', 'bark_send');
+        Typecho_Plugin::factory('Widget_Feedback')->trackback = array('Comment2Bark_Plugin', 'bark_send');
+        Typecho_Plugin::factory('Widget_XmlRpc')->pingback = array('Comment2Bark_Plugin', 'bark_send');
         
-        return _t('请配置此插件的 KEY, 以使您的Bark推送生效');
+        return _t('请配置此插件的 Bark Key, 以使您能顺利推送到Bark');
     }
     
     /**
@@ -34,7 +31,7 @@ class Comment2Bark_Plugin implements Typecho_Plugin_Interface
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate(){}
+    public static function deactivate() {}
     
     /**
      * 获取插件配置面板
@@ -43,11 +40,36 @@ class Comment2Bark_Plugin implements Typecho_Plugin_Interface
      * @param Typecho_Widget_Helper_Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form)
-    {
-        $key = new Typecho_Widget_Helper_Form_Element_Text('barkkey', NULL, NULL, _t('BARKKEY'), _t('BARKKEY需要在App Store商店搜索【Bark】，提取https://api.day.app/后边的字符串
-        填写相应的BarkKey才能收到推送'));
-        $form->addInput($key->addRule('required', _t('您必须填写一个正确的 BarkKEY')));
+    public static function config(Typecho_Widget_Helper_Form $form) {
+        $key = new Typecho_Widget_Helper_Form_Element_Text('bark_key', NULL, NULL, _t('Bark Key'), _t('下载 Bark App 后获取Bark key'));
+        $form->addInput($key->addRule('required', _t('您必须填写一个正确的 Bark Key')));
+        
+        $icon = new Typecho_Widget_Helper_Form_Element_Text('bark_icon', NULL, NULL, _t('Icon'), _t('（非必填）自定义 Bark 推送图标，图标需为 URL 链接'));
+        $form->addInput($icon);
+        
+        $group = new Typecho_Widget_Helper_Form_Element_Text('bark_group', NULL, NULL, _t('Group'), _t('（非必填）自定义 Bark 消息分组'));
+        $form->addInput($group);
+        
+        $sound = new Typecho_Widget_Helper_Form_Element_Text('bark_sound', NULL, NULL, _t('Sound'), _t('（非必填）自定义 Bark 消息提示音，参考https://github.com/Finb/Bark/tree/master/Sounds'));
+        $form->addInput($sound);
+        
+        $server = new Typecho_Widget_Helper_Form_Element_Text('bark_server', NULL, NULL, _t('Server'), _t('（非必填,默认官方：https://api.day.app/）自定义 Bark 服务器'));
+        $form->addInput($server);
+        
+        $archive = new Typecho_Widget_Helper_Form_Element_Text('bark_archive', NULL, NULL, _t('Archive'), _t('（非必填）自定义 Bark 消息保存，值为 1 时保存消息，其他值为不保存，不填则保持默认<br><br>'));
+        $form->addInput($archive);
+
+        $notMyself = new Typecho_Widget_Helper_Form_Element_Radio('notMyself',
+            array(
+                '1' => '是',
+                '0' => '否'
+            ),'1', _t('当评论者为自己时不发送通知'), _t('启用后，若评论者为博主，则不会向 Bark 发送通知，若博主 UID 不为 1，则需要在下方填写博主的 UID'));
+        $form->addInput($notMyself);
+        
+        $customUid = new Typecho_Widget_Helper_Form_Element_Text('customUid', NULL, NULL, _t('自定义博主 UID'), _t('（非必填）自定义博主 UID<br><br><br>
+            此插件由原作者 <a href="https://yian.me">Y!an</a> 的 <a href="https://github.com/YianAndCode/Comment2Bark">Comment2Bark 1.0.0</a> 和 <a href="https://moe.best">神代綺凛</a> 的 <a href="https://github.com/Tsuk1ko/Comment2Wechat">Comment2Bark 2.0.0</a>和JDYuuki的<a href="https://github.com/JDYuuki/Comment2Bark">Comment2Bark
+            </a>插件修改而来<br>本插件项目地址：<a href="https://github.com/isSuperman/Typecho-Plugin-Comment2Bark">https://github.com/isSuperman/Typecho-Plugin-Comment2Bark</a>'));
+        $form->addInput($customUid);
     }
     
     /**
@@ -60,24 +82,56 @@ class Comment2Bark_Plugin implements Typecho_Plugin_Interface
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
     /**
-     * Bark推送
+     * 推送到Bark
      * 
      * @access public
      * @param array $comment 评论结构
      * @param Typecho_Widget $post 被评论的文章
      * @return void
      */
-    public static function sc_send($comment, $post)
-    {
-        $options = Typecho_Widget::widget('Widget_Options');
+    public static function bark_send($comment, $post) {
+        $options = Typecho_Widget::widget('Widget_Options')->plugin('Comment2Bark');
 
-        $barkkey = $options->plugin('Comment2Bark')->barkkey;
+        $bark_key = $options->bark_key;
+        $bark_icon = $options->bark_icon;
+        $bark_group = $options->bark_group;
+        $bark_server = $options->bark_server;
+        $bark_sound = $options->bark_sound;
+        $bark_archive = $options->bark_archive;
 
-		$url = 'https://api.day.app';
-		
-		$title = '有人在你的博客发表了评论';
+        $notMyself = $options->notMyself;
+        $customUid = $options->customUid;
+        
+        // 判断是否启用当评论者为自己时不发送通知
+        if($notMyself == '1') {
+            if (!empty($customUid)) {
+                if ($comment['authorId'] == $customUid) {
+                    return $comment;
+                }
+            } elseif ($comment['authorId'] == 1) {
+                return  $comment;
+            }
+        }
+        
+        $text = "iColak's blog 新评论";
+        $desp = urlencode($comment['author']." 在「".$post->title."」中说：").'%0a'.urlencode($comment['text']).'%0a%0a'.$post->permalink;
 
-		$result = file_get_contents($url.'/'.$barkkey.'/'.$title.'/'.$comment['author'].'在「'.$post->title.'」说到:'.$comment['text']);
+        !empty($bark_server) ? $bark_server = $bark_server : "https://api.day.app/";
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => 'title='.$text.'&body='.$desp.'&icon='.$bark_icon.'&group='.$bark_group.'&archive='.$bark_archive.'&sound='.$bark_sound
+            ),
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false
+            )
+        );
+
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($bark_server.$bark_key, false, $context);
         
         return  $comment;
     }
--- README.md
+++ README.md
@@ -3,13 +3,22 @@
 > Typecho 插件——新评论Bark通知 
 
 ## 感谢
-本插件的推送采用了 [Fin](https://github.com/Finb/Bark) 的 [Bark]解决方案.
+1. 本插件的推送采用了 [Fin](https://github.com/Finb/Bark) 的 [Bark]解决方案.
+2. 1.2版本代码由![Comment2Bark](https://github.com/JDYuuki/Comment2Bark)修改而来
 
 ## 使用方法
 
  1. [点此下载](https://github.com/Colaink/Comment2Bark/archive/master.zip)，将解压后的目录名改为 `Comment2Bark`，然后上传到你的 Typecho 的 `/usr/plugins`，并在 Typecho 后台开启插件
  2. 在App Store商店搜索【Bark】，下载打开后，提取https://api.day.app/后边的字符串
  3. 将你 `BARKKEY` 填到插件设置里，保存即可
+
+## 配置选项
+1. Bark Key - 必填项
+2. Icon - （非必填）非必填推送消息图标URL，需iOS15+
+3. Group - （非必填）推送消息分组
+4. Sound - （非必填）推送铃声，具体参考 https://github.com/Finb/Bark/tree/master/Sounds
+5. Server - （非必填）自建服务器需要填写，必须以**'/'**结尾
+6. Archive - （非必填）值为1时保存，其他值为不保存，不填则保持默认
 
 
 ## 截图
@@ -19,3 +28,6 @@
 ## 更新记录
  - v1.0
    - init
+ - v1.1
+   - 适配最新版本Typecho1.2和PHP8+
+   - 增加配置项，使用更灵活
